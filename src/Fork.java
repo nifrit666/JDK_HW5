422aef6e39891ed0781ff1739682dadc2e767ef6
«Andrey_Istomin» <an.istomin@gmail.com>
2024-08-07 23:03:18 +0500

    Add code files

---
+++ src/Fork.java
@@ -0,0 +1,12 @@
+
+public class Fork {
+    private boolean using;
+
+    public boolean isUsing() {
+        return using;
+    }
+
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}
